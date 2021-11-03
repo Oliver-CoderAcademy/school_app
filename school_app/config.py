571eee09f7753a29cc1ceb0c5eb6ea1b2083a456
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2021-11-03 18:43:15 +1000

    converted DB_URI to property

--- school_app/config.py
+++ school_app/config.py
@@ -1,21 +1,25 @@
 import os
-class Config(object):
-    
-    URI_VARS = [
-        "DB_USER", 
-        "DB_PASS", 
-        "DB_NAME", 
-        "DB_DOMAIN"
-        ]
-    
-    uri_dict = {item: os.environ.get(item) for item in URI_VARS}
-    
-    for key in URI_VARS:
-        if uri_dict[key] is None:
-            raise ValueError(f"{key} is not set.")
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{uri_dict['DB_USER']}:{uri_dict['DB_PASS']}@{uri_dict['DB_DOMAIN']}/{uri_dict['DB_NAME']}"
+class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        URI_VARS = [
+            "DB_USER", 
+            "DB_PASS", 
+            "DB_NAME", 
+            "DB_DOMAIN"
+        ]
+        
+        uri_dict = {item: os.environ.get(item) for item in URI_VARS}
+        
+        for key in URI_VARS:
+            if uri_dict[key] is None:
+                raise ValueError(f"{key} is not set.")
+
+        return f"postgresql+psycopg2://{uri_dict['DB_USER']}:{uri_dict['DB_PASS']}@{uri_dict['DB_DOMAIN']}/{uri_dict['DB_NAME']}"
+        
 
 class DevelopmentConfig(Config):
     DEBUG = True
